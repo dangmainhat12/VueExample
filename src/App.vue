a8eddb299471b36ca2dadef09a1754c0615f5593
dangmainhat12 <mainhatdangthi@gmail.com>
2022-05-12 17:33:34 +0700

    vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
+    },
+    notify() {
+      alert('navigation was prevented.')
+    }
+  }
+}
+</script>
+
+<template>
+  
+  <h1>{{ message }}</h1>
+  <button @click="reverseMessage">Reverse Message</button>
+  <button @click="message += '!'">Append "!"</button>
+  <a href="https://vuejs.org" @click.prevent="notify">
+    A link with e.preventDefault()
+  </a>
+</template>
+
+<style>
+button, a {
+  display: block;
+  margin-bottom: 1em;
+}
+</style>
