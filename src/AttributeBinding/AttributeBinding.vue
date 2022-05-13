ecd01c09f328d84ce181b3781a6b3bfa59c7b443
dangmainhat12 <mainhatdangthi@gmail.com>
2022-05-13 13:22:59 +0700

    Attribute

---
+++ src/AttributeBinding/AttributeBinding.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!',
+      isRed: true,
+      color: 'green'
+    }
+  },
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed
+    },
+    toggleColor() {
+      this.color = this.color === 'green' ? 'blue' : 'green'
+    }
+  }
+}
+</script>
+
+<template>
+  <p><span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!</span></p>
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... but click me to toggle it.
+  </p>
+  <p :style="{ color }" @click="toggleColor">
+    This should be green, and should toggle between green and blue on click.
+  </p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
