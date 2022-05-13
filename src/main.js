4341fc003a1de87bb1e4f94f62ec1246706faac0
dangmainhat12 <mainhatdangthi@gmail.com>
2022-05-13 10:46:03 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import App from './HandingInput/App.vue'
-import App from './FormBinding.vue'
-import App from './ConditionalLoops.vue'
-import App from './AttributeBinding.vue'
+// import App from './HandingInput/App.vue'
+// import App from './FormBinding.vue'
+// import App from './ConditionalLoops.vue'
+// import App from './AttributeBinding.vue'
 
 createApp(App).mount('#app')
