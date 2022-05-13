650f1cc1b4e52486a30ee1859d3fc8fb154ca8a0
dangmainhat12 <78189787+dangmainhat12@users.noreply.github.com>
2022-05-13 14:37:58 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,73 @@
 <script>
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+
 export default {
-  data() {
-    return {
-      message: 'Hello World!'
-    }
+  data: () => ({
+    branches: ['main', 'v2-compat'],
+    currentBranch: 'main',
+    commits: null
+  }),
+
+  created() {
+    // fetch on init
+    this.fetchData()
   },
+
+  watch: {
+    // re-fetch whenever currentBranch changes
+    currentBranch: 'fetchData'
+  },
+
   methods: {
-    reverseMessage() {
-      this.message = this.message.split('').reverse().join('')
+    async fetchData() {
+      const url = `${API_URL}${this.currentBranch}`
+      this.commits = await (await fetch(url)).json()
     },
-    notify() {
-      alert('navigation was prevented.')
+    truncate(v) {
+      const newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
+    },
+    formatDate(v) {
+      return v.replace(/T|Z/g, ' ')
     }
   }
 }
 </script>
 
 <template>
-  
-  <h1>{{ message }}</h1>
-  <button @click="reverseMessage">Reverse Message</button>
-  <button @click="message += '!'">Append "!"</button>
-  <a href="https://vuejs.org" @click.prevent="notify">
-    A link with e.preventDefault()
-  </a>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input type="radio"
+      :id="branch"
+      :value="branch"
+      name="branch"
+      v-model="currentBranch">
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul>
+    <li v-for="{ html_url, sha, author, commit } in commits">
+      <a :href="html_url" target="_blank" class="commit">{{ sha.slice(0, 7) }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span><br>
+      by <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
 </template>
 
 <style>
-button, a {
-  display: block;
-  margin-bottom: 1em;
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
 }
 </style>
