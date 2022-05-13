7934230a0e417aac04e5745184d4cecc6c96020d
dangmainhat12 <mainhatdangthi@gmail.com>
2022-05-13 14:01:10 +0700

    u

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-// import App from './HandingInput/App.vue'
-// import App from './FormBinding.vue'
-// import App from './ConditionalLoops.vue'
-// import App from './AttributeBinding.vue'
+import App from './FetchingData.vue'
 
 createApp(App).mount('#app')
