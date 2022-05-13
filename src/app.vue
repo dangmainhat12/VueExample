b39c9c7e92716b055e8fd7f958a43a2e742a33ec
dangmainhat12 <78189787+dangmainhat12@users.noreply.github.com>
2022-05-13 15:01:43 +0700

    Add files via upload

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  }
+}
+</script>
+
+<template>
+  <button id="show-modal" @click="showModal = true">Show Modal</button>
+
+  <Teleport to="body">
+    <modal :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>custom header</h3>
+      </template>
+    </modal>
+  </Teleport>
+</template>
